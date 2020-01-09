40732f8d4371fd61cbb7e09cb0475bd52d7bff73
wenxin45445 <1211694581@qq.com>
2020-01-10 00:42:19 +0800

    支付宝配置信息

--- meng-pay/src/main/java/com/meng/config/AlipayConfig.java
+++ meng-pay/src/main/java/com/meng/config/AlipayConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
+ * @author mengpp
  * 类名：AlipayConfig
  * 功能：基础配置类
  * 详细：设置帐户有关信息及返回路径
@@ -12,40 +13,62 @@ import org.springframework.stereotype.Component;
  * 以下代码只是为了方便商户测试而提供的样例代码，商户可以根据自己网站的需要，按照技术文档编写,并非一定要使用该代码。
  * 该代码仅供学习和研究支付宝接口使用，只是提供一个参考。
  */
+
 @Component
 public class AlipayConfig {
 
-    // 应用ID,您的APPID，收款账号既是您的APPID对应支付宝账号
+    /**
+     *  应用ID,您的APPID，收款账号既是您的APPID对应支付宝账号
+     */
     @Value("${alipay.appId}")
     private String appId;
 
-    // 商户私钥，您的PKCS8格式RSA2私钥
+    /**
+     * 商户私钥，您的PKCS8格式RSA2私钥
+     */
     @Value("${alipay.privateKey}")
     private  String privateKey;
 
-    // 支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
+    /**
+     * 支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
+     */
     @Value("${alipay.publicKey}")
     private String publicKey;
 
-    // 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
+    /**
+     * 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
+     */
     @Value("${alipay.notifyUrl}")
     private String notifyUrl;
 
-    // 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
+    /**
+     * 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
+     */
     @Value("${alipay.returnUrl}")
     private String returnUrl;
 
-    // 签名方式
+    /**
+     * 签名方式
+     */
     @Value("${alipay.signType}")
     private String signType;
 
-    // 字符编码格式
+    /**
+     * 字符编码格式
+     */
     @Value("${alipay.charset}")
     private String charset;
 
-    // 支付宝网关
+    /**
+     * 支付宝网关
+     */
     @Value("${alipay.gatewayUrl}")
     private String gatewayUrl;
+
+    /**
+     * AES key
+     */
+    private String aesKey;
 
     public String getAppId() {
         return appId;
@@ -109,6 +132,14 @@ public class AlipayConfig {
 
     public void setGatewayUrl(String gatewayUrl) {
         this.gatewayUrl = gatewayUrl;
+    }
+
+    public String getAesKey() {
+        return aesKey;
+    }
+
+    public void setAesKey(String aesKey) {
+        this.aesKey = aesKey;
     }
 }
 
